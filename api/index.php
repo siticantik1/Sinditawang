49e8c74b33b473b61bb75c4c90d259f95e2297ad
siticantik1 <siticantik1@example.com>
2025-10-16 13:36:25 +0700

    Add Vercel configuration

---
+++ api/index.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
+ */
+
+// Arahkan ke file index.php publik milik Laravel
+require __DIR__ . '/../public/index.php';
